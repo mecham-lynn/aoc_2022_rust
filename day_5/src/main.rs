8d2e778d88505d6b61ee6e1b4f332ead0c0024f4
Lynn Mecham <lmecham@commercehub.com>
2022-12-07 13:22:41 -0700

    day_5 cont'd

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -17,34 +17,46 @@ fn main() {
     
     let mut container_stacks: Vec<Vec<char>> = vec![];
 
-
-    let mut add_next: bool = false;
     
-    for ( stack, line) in container_structure.lines().enumerate() {
-        let mut location_on_stack = 0_usize;
+    for line in container_structure.lines() {
+        let mut row = vec![];
+        let last_char = line.chars().last();
         for (location, char) in line.chars().enumerate() {
             if location == 0 && char.is_ascii_whitespace() {
-                container_stacks[stack][location_on_stack] = ' ';
-                location_on_stack += 1;
+                row.push(' ');
             } else if char.is_ascii_alphabetic() {
-                container_stacks[stack][location_on_stack] = char
+                row.push(char)
             }
         }
+        if last_char.unwrap().is_ascii_whitespace() {
+            row.push(' ');
+        }
+        // evaluate row to see if the whole thing is empty chars
+        let eval = row.iter().filter(|&a| !a.is_ascii_whitespace()).collect::<Vec<&char>>();
+        if !eval.is_empty(){
+            container_stacks.push(row);
+        }
+
     }
-    //container structure parsing
-    // let containers:Vec<Vec<char>> = container_structure.split("\n")
-    //     .inspect(|a| println!("current container string {a}"))
-    //     .map(|a| a
-    //         .split_terminator(&['[',']'])
-    //         .collect::<Vec<&str>>()
-    //         .iter()
-    //         .flat_map(|c| c.chars())
-    //         .collect()
-    //     )
-    //     .collect();
+
 
     println!("Containers = {:?}",container_stacks);
 
+    // create the commands
+        let command = commands
+        .lines()
+        .map(|a| a
+            .split_whitespace()
+            .flat_map(|a|a
+                .chars())
+            .filter_map(|a| 
+                    a.to_string().parse::<i32>().ok()
+            )
+            .collect::<Vec<i32>>()
+        ).map(|a| )
+        
+        
+        println!("{command:?}");
 
     println!("executed in {}mc", now.elapsed().as_micros());
 
@@ -54,4 +66,43 @@ pub struct OperatorCommand {
     move_amount: i32,
     from_stack: i32,
     to_stack: i32,
+}
+
+impl OperatorCommand {
+    fn new(container_amount: i32, from_stack: i32, to_stack: i32) -> Self {
+        Self {
+            move_amount: container_amount,
+            from_stack,
+            to_stack,
+        }
+    }
+
+    fn execute(&self, container_stacks: &mut Vec<Vec<char>>) {
+        
+        let from_stack_id = {self.from_stack - 1} as usize;
+        let to_stack_id = {self.to_stack - 1} as usize;
+        // while we have `move_amount` left should keep moving
+        let mut move_count = 0;
+        while move_count < self.move_amount {
+            let new_container_stack = container_stacks.clone();
+            // grab the container from it's existing stack
+            let (stack_depth, container) = {
+                new_container_stack[from_stack_id].iter().enumerate().find(|(_s, &container)| container.is_alphabetic()).unwrap()
+               
+            };
+            let new_stack_depth = container_stacks[to_stack_id].iter().enumerate().find(|(_s, &container)| container.is_alphabetic()).unwrap().0 -1;
+
+            container_stacks[from_stack_id][stack_depth] = ' ';
+            container_stacks[to_stack_id][new_stack_depth] = container.clone();
+            move_count +=1;
+        }
+
+
+    }
+}
+
+fn print_stack(container_stack: &Vec<Vec<char>>) {
+    for row in container_stack {
+        println!("{row:?}")
+    }
 }
