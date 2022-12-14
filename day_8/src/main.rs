0b99a39a50cee092a4fb857d197039ac33e433a6
Lynn Mecham <lmecham@commercehub.com>
2022-12-14 13:00:14 -0700

    day_8 complete

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -11,9 +11,110 @@ fn main() {
         include_str!("../input.txt")
     };
 
-    // get the tree heights into a 
+    // get the tree heights into a vec
+    
 
+    let mut visible_trees = 0;
+    let mut scenic_scores = vec![];
+    let trees = input_txt.lines().map(|a| a.chars().map(|a| a.to_digit(10).unwrap()).collect::<Vec<u32>>()).collect::<Vec<Vec<u32>>>();
 
+    let row_max = trees.len();
+    let col_max = trees[0].len();
+
+    // Check up down left and right (if possible) and iterate visible trees when the tree < all trees in that direction to the edge
+    // This means a tree is visible if every tree is shorter than it in one direction  to the edge.
+    for (row_id, col) in trees.iter().enumerate() {
+    
+        if row_id == 0 || row_id == trees.len() - 1 {
+                visible_trees += col.len()
+        } else {
+
+            for (col_id, tree) in col.iter().enumerate() {
+                let mut to_right = vec![];
+                let mut to_left = vec![];
+                let mut to_down = vec![];
+                let mut to_up = vec![];
+                
+                if col_id == 0 || col_id == col_max - 1 {
+                    visible_trees += 1;
+
+                } else {
+                    //check right
+                    if col_id < col_max {
+                        for i in col_id + 1..col_max {
+                            to_right.push(trees[row_id][i])
+                        }
+                    }
+
+                    //check left
+                    if col_id > 0 {
+                        for i in 0..col_id {
+                            to_left.push(trees[row_id][i])
+                        }
+                        to_left.reverse()
+                    }
+
+                    // check down
+                    if row_id > 0 {
+                        for i in row_id + 1..row_max {
+                            to_down.push(trees[i][col_id])
+                        }
+                    }
+
+                    // check up
+                    if row_id < row_max {
+                        for i in (0..row_id).rev() {
+                            to_up.push(trees[i][col_id]);
+                        }
+                    }
+                    
+                    if is_visible(tree, &to_right) || is_visible(tree, &to_left) || is_visible(tree, &to_down) || is_visible(tree, &to_up) {
+                        visible_trees += 1
+                    }
+
+                    // let up_scenic_score = scenic_score(tree, &to_up);
+                    // let down_scenic_score = scenic_score(tree, &to_down);
+                    // let right_scenic_score = scenic_score(tree, &to_right);
+                    // let left_scenic_score = scenic_score(tree, &to_left);
+
+                    // println!("to_up: {:?}", &to_up);
+                    // println!("tree {}, up: {}, down {}, left {}, right {}", &tree, up_scenic_score, down_scenic_score, left_scenic_score, right_scenic_score);
+                    // println!("total_scenic score {}", up_scenic_score * down_scenic_score * left_scenic_score * right_scenic_score);
+
+                    scenic_scores.push(scenic_score(tree, &to_right) * scenic_score(tree, &to_left) * scenic_score(tree, &to_down) * scenic_score(tree, &to_up));
+                }
+            }
+        }
+    }
+    
+    println!("Number of visible trees {}", visible_trees);
+    // println!("scenic scores {:?}",&scenic_scores );
+    println!("Max scenic score {:?}", scenic_scores.iter().max());
 
     println!("executed in {}mc", now.elapsed().as_micros());
+}
+
+fn is_visible(tree: &u32, slice: &[u32] ) -> bool {
+    // if slice.is_empty(){
+    //     return false
+    // }
+    // println!("tree evaluating: {}, against trees {:?}", tree, slice);
+    let result = slice.iter().all(|a| tree > a);
+    // println!("evaluated to {}", result);
+    result
+}
+
+fn scenic_score(tree: &u32, slice: &[u32]) -> usize {
+    let mut count = 0;
+    for o_tree in slice {
+        if o_tree < tree {
+            count += 1
+        } else  if o_tree >= tree {
+            count += 1;
+            break
+        } else {
+            break
+        }
+    }
+    count
 }
--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -4,14 +4,14 @@ use argh::FromArgs;
 /// Set of args for Day 3
 pub struct AocArgs{
     /// enables part_one functionality
-    #[argh(switch)]
+    #[argh(switch, short='o')]
     pub part_one: bool,
 
     /// enables part_two functionality
-    #[argh(switch)]
+    #[argh(switch, short='t')]
     pub part_two: bool,
 
     /// flag on whether to use demo text or the actual text
-    #[argh(switch)]
+    #[argh(switch, short = 'd')]
     pub demo_text: bool,
 }
